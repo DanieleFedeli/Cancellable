60a1235784ccfdb5cdddbb48ff0bb8eec1ecfb3f
Daniele Fedeli <danielefedeli@MacBook-Air.station>
2023-10-07 20:51:45 +0200

    Fixed CI

--- tests/cancellable.spec.js
+++ tests/cancellable.spec.js
@@ -1,5 +1,5 @@
 const t = require("tap");
-const Cancellable = require(".");
+const { Cancellable } = require("../lib");
 
 t.test("Aborted promise", async (t) => {
   t.plan(1);
@@ -30,30 +30,29 @@ t.test("Resolved promise with abortController", async (t) => {
   t.plan(1);
   const fn = new Promise((r) => setImmediate(r));
   const abortController = new AbortController();
-  const promise  = Cancellable({promise: fn, abortController});
+  const promise = Cancellable({ promise: fn, abortController });
 
   await promise;
   t.pass();
 });
 
-
 t.test("Aborted before awaiting", async (t) => {
   t.plan(1);
   const fn = new Promise((r) => setImmediate(r));
   const ac = new AbortController();
-  ac.abort('Aborted')
+  ac.abort("Aborted");
 
-  const promise = Cancellable({promise:fn, abortController: ac});
+  const promise = Cancellable({ promise: fn, abortController: ac });
 
   try {
-    await promise
+    await promise;
     t.fail("Should thrown");
   } catch (error) {
     t.pass();
   }
 });
 
-t.test("Not passing a promise", t => {
+t.test("Not passing a promise", (t) => {
   t.plan(1);
   try {
     Cancellable({});
@@ -61,4 +60,4 @@ t.test("Not passing a promise", t => {
   } catch (error) {
     t.pass();
   }
-})
+});
